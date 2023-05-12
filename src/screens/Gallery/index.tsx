f6eb0d9dac1ebc49bd34496f3d2d5e66343518b7
Vahid Hasani <vahidhasani09@gmail.com>
2023-05-12 12:47:47 +0330

    Add gallery screen

--- src/screens/Gallery/index.tsx
+++ src/screens/Gallery/index.tsx
@@ -1,15 +1,31 @@
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {useRoute} from '@react-navigation/native';
 import React from 'react';
-import {ActivityIndicator, View} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import {FlatList, Image, Pressable, SafeAreaView, View} from 'react-native';
+import {useAppNavigation} from '../../hooks/useNavigate';
+import {RouteProp} from '../../types/screen';
 import styles from './styles';
 
 function Gallery() {
+  const route = useRoute<RouteProp<'Gallery'>>();
+  const navigation = useAppNavigation();
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={[styles.container, styles.horizontal]}>
-        <ActivityIndicator />
+    <SafeAreaView>
+      <View style={styles.backContainer}>
+        <Pressable onPress={() => navigation.goBack()} hitSlop={23}>
+          <FontAwesomeIcon icon={faChevronLeft} size={23} />
+        </Pressable>
       </View>
-    </ScrollView>
+      <FlatList
+        contentContainerStyle={styles.container}
+        keyExtractor={item => item}
+        data={route.params.images}
+        renderItem={({item}) => (
+          <Image source={{uri: item}} style={styles.image} />
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
--- src/screens/Gallery/styles.ts
+++ src/screens/Gallery/styles.ts
@@ -3,56 +3,22 @@ import {Dimensions, StyleSheet} from 'react-native';
 const {height} = Dimensions.get('window');
 
 export default StyleSheet.create({
-  container: {margin: 28},
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
+  container: {
+    margin: 20,
   },
-
-  image: {
-    flex: 1,
-    justifyContent: 'space-between',
-    height: height / 2 - 50,
-    padding: 20,
-  },
-  roundImage: {
-    borderRadius: 20,
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  iconContainer: {
+  backContainer: {
+    position: 'absolute',
+    top: 42,
+    left: 42,
+    zIndex: 9999,
     backgroundColor: '#fff',
-    color: '#000',
-    padding: 15,
-    borderRadius: 999,
+    borderRadius: 9999,
+    padding: 12,
   },
-  icon: {
-    width: 25,
-    height: 25,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-
-  titleContainer: {
-    marginTop: 24,
-    paddingHorizontal: 5,
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 15,
-    backgroundColor: 'rgba(255,255,255,0.35)',
-  },
-  thumbnail: {
-    height: 50,
-    width: 50,
-    margin: 8,
-    borderRadius: 10,
+  image: {
+    height: height / 2 - 80,
+    padding: 20,
+    marginBottom: 20,
+    borderRadius: 25,
   },
 });
